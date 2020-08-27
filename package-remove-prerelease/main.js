c85265a4aec27cf006ee06ff2c03b6f116118b0a
emilsivervik <emilsivervik@users.noreply.github.com>
2020-08-27 14:56:04 +0000

    1.3.1-pr10.26

--- package-remove-prerelease/main.js
+++ package-remove-prerelease/main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { exec } = require("@actions/exec");
 const { getInput, setFailed, setOutput, debug, setSecret } = require("@actions/core");
 const { Octokit } = require("@octokit/rest");
