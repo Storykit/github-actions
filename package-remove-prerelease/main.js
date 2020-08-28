82ed3268efd1446158bb36a7d703935107eff41b
emilsivervik <emilsivervik@users.noreply.github.com>
2020-08-28 09:32:08 +0000

    1.3.1-pr10.40

--- package-remove-prerelease/main.js
+++ package-remove-prerelease/main.js
@@ -5,8 +5,8 @@ const { getInput, setFailed, setOutput, debug } = require("@actions/core");
 const { Octokit } = require("@octokit/rest");
 const { context } = require('@actions/github')
 
-const owner = context.payload.organization.login;
-const repo = context.payload.repository.name;
+const owner = context.payload.organization.login.toLocaleLowerCase();
+const repo = context.payload.repository.name.toLocaleLowerCase();
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const pullRequestFix = getInput('pull-request-fix');
@@ -31,10 +31,13 @@ const run = async () => {
       INPUT_TOKEN: process.env.NODE_AUTH_TOKEN
     }
 
+    let removedGitTags = [];
+    let removedNpmVersions = [];
+
     const tagsToRemovePromise = tags.map(tag => {
       const options = {
-        owner: owner.toLocaleLowerCase(),
-        repo: repo.toLocaleLowerCase(),
+        owner: owner,
+        repo: repo,
         ref: `tags/${tag}`,
         headers: {
           authorization: `token ${GITHUB_TOKEN}`,
@@ -43,9 +46,13 @@ const run = async () => {
       return git.request('DELETE /repos/:owner/:repo/git/refs/:ref', options)
         .then((response) => {
           if (response.status === 204) {
-            return exec(`npm deprecate @${owner}/${repo}@${tag} "Beta release deprecated, please use latest version."`, { env })
-              .catch(err => (debug(err)));
+            removedGitTags.push(tag);
             debug(`Removed tag: ${tag}`);
+            return exec(`npm deprecate @${owner}/${repo}@${tag} "Beta release deprecated, please use latest version."`, { env })
+              .then(() => {
+                removedNpmVersions.push(tag);
+              })
+              .catch(err => (debug(err)));
           } else {
             debug(`Status: ${response.status}`)
             debug(`Was not able to remove tag: ${tag}`);
@@ -59,7 +66,8 @@ const run = async () => {
 
     await Promise.all(tagsToRemovePromise)
 
-    setOutput('removed-releases', tags.join());
+    setOutput('removed-git-tags', removedGitTags.join());
+    setOutput('removed-npm-versions', removedNpmVersions.join());
   } catch (err) {
     setFailed(err)
   }
