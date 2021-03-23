1f9b45e33e5ba1013205077a9d1cba36b71e821d
Daniel Rönnkvist <daniel.ronnkvist@storykit.io>
2021-03-23 09:03:02 +0100

    feat: heroku docker release action

---
+++ heroku-docker-release/README.md
@@ -0,0 +1,13 @@
+Heroku Docker Release
+=======
+
+Build and release docker images to heroku.
+
+## Action Options:
+
+|Parameter|Description|Default Value|Required|
+|:---:|---|---|:---:|
+|`heroku-app-name`|The name of your heroku app name||:white_check_mark:|
+|`heroku-user-email`|Heroku user email||:white_check_mark:|
+|`heroku-api-key`|API key for your heroku user||:white_check_mark:|
+|`heroku-app-formation`|Formation key|`web`||
---
+++ heroku-docker-release/main.js
@@ -0,0 +1,59 @@
+const { exec } = require("@actions/exec");
+const { getInput, setFailed, debug } = require("@actions/core");
+
+async function buildDockerImage() {
+  try {
+    const appName = getInput("heroku-app-name");
+    const formation = getInput("heroku-app-formation");
+    const docker = `docker build -t registry.heroku.com/${appName}/${formation} --build-arg NPM_TOKEN=${process.env.NPM_TOKEN} .`;
+    debug(docker);
+    await exec(docker);
+  } catch (err) {
+    setFailed(`failed building docker image: ${err}`);
+  }
+}
+
+async function loginToHeroku() {
+  try {
+    const password = getInput("heroku-api-key");
+    const login = getInput("heroku-user-email");
+    await exec(
+      `echo ${password} | docker login --username=${login} registry.heroku.com --password-stdin`
+    );
+  } catch (err) {
+    setFailed(`failed logging in to heroku: ${err}`);
+  }
+}
+
+async function pushToHeroku() {
+  try {
+    const password = getInput("heroku-api-key");
+    const appName = getInput("heroku-app-name");
+    await exec(
+      `HEROKU_API_KEY=${password} heroku container:push web --app ${appName}`
+    );
+  } catch (err) {
+    setFailed(`failed pushing to heroku: ${err}`);
+  }
+}
+
+async function releaseToHeroku() {
+  try {
+    const password = getInput("heroku-api-key");
+    const appName = getInput("heroku-app-name");
+    await exec(
+      `HEROKU_API_KEY=${password} heroku container:release web --app ${appName}`
+    );
+  } catch (err) {
+    setFailed(`failed releasing to heroku: ${err}`);
+  }
+}
+
+async function run() {
+  await buildDockerImage();
+  await loginToHeroku();
+  await pushToHeroku();
+  await releaseToHeroku();
+}
+
+run();
